629e011180e79d9e0aa4c3b46c4db3bb35b12e4e
David Alonso López <davidal@compusoftgroup.com>
2024-04-06 13:33:20 +0200

    Initial commit

---
+++ UrlShortener_Backend/DataAccess/Models/Url.cs
@@ -0,0 +1,17 @@
+﻿namespace UrlShortener_Backend.DataAccess.Models
+{
+    public class Url
+    {
+        public Url()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public string OriginalUrl { get; set; }
+        public string ShortUrl { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int NumberOfVisits { get; set; }
+    }
+}
+
---
+++ UrlShortener_Backend/DataAccess/UrlShortenerDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace UrlShortener_Backend.DataAccess
+{
+    public class UrlShortenerDbContext : DbContext
+    {
+        public UrlShortenerDbContext(DbContextOptions<UrlShortenerDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Models.Url> Urls { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Models.Url>()
+                .HasKey(u => u.Id);
+
+            modelBuilder.Entity<Models.Url>()
+                .Property(u => u.Id)
+                .UseMySqlIdentityColumn()
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Models.Url>()
+                .Property(u => u.CreatedOn)
+                .ValueGeneratedOnAdd();
+        }
+    }
+
+}
